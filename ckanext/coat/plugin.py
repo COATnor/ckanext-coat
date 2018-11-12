e57e701554f01bce2523fa7bf365506b1d0ddba9
Francesco Frassinelli <francesco.frassinelli@nina.no>
2018-11-12 14:23:08 +0100

    Fix git tracking

--- ckanext/coat/plugin.py
+++ ckanext/coat/plugin.py
@@ -33,18 +33,23 @@ class CoatPlugin(plugins.SingletonPlugin):
         repo = git.Repo.init(directory)
 
     def _add_file(self, context, resource, directory, delete=False):
+        if resource['url_type'] != 'upload':
+            return
+        repo = git.Repo(directory)
         name = resource['name']
-        # same name condition
         filename = os.path.join(directory, name)
+        if os.path.isfile(filename):
+            extra = '-' + resource['id']
+            name += extra
+            filename += extra
         if delete:
-            os.remove.filename(filename)
-            message = "Resource deleted: "+name
+            os.remove(filename)
+            repo.index.remove([name])
+            message = "Remove: " + name
         else:
             urllib.request.urlretrieve(resource['url'], filename)
-            message = "Resource added: "+name
-        repo = git.Repo(directory)
-        # if not txt ... LFS
-        repo.index.add([name])
+            repo.index.add([name])
+            message = "Add: " + name
         display_name = context['auth_user_obj'].display_name
         email = context['auth_user_obj'].email
         author = git.Actor(display_name, email)
@@ -65,16 +70,10 @@ class CoatPlugin(plugins.SingletonPlugin):
         else:
             self._create_repository(context, obj, directory)
 
-    def after_update(self, context, obj):
-        directory = self._get_directory(context, obj)
-        if _is_resource(obj):
-            self._add_file(c0ontext, obj, directory)
-        else:
-            pass
-
-    def after_delete(self, context, obj):
-        directory = self._get_directory(context, obj)
-        if _is_resource(obj):
-            self._delete_file(context, obj, directory)
-        else:
-            pass # empty repo?
+    def before_delete(self, context, uid, objs):
+        for obj in objs:
+            directory = self._get_directory(context, obj)
+            if _is_resource(obj):
+                self._add_file(context, obj, directory, delete=True)
+            else:
+                pass # empty repo?
