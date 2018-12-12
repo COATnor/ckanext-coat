4299aa0337d4ac17a3cf9ce4a152a062f865098c
Francesco Frassinelli <francesco.frassinelli@nina.no>
2018-12-12 10:45:05 +0100

    Add comments and improve next version code

--- ckanext/coat/logic/action/create.py
+++ ckanext/coat/logic/action/create.py
@@ -4,27 +4,34 @@ from ckanext.coat.helpers import extras_dict
 
 @toolkit.side_effect_free
 def package_create(context, data_dict):
+    # parent dataset
+    # https://github.com/aptivate/ckanext-datasetversions/issues/10
     if len(data_dict) <= 3:
         return ckan_package_create(context, data_dict)
+
+    # set base_name extra field
     base_name = data_dict['name']
     data_dict.setdefault('extras', [])
     if 'base_name' not in extras_dict(data_dict):
         data_dict['extras'].append(
             {'key': 'base_name', 'value': base_name},
          )
+
+    # set version
     if not data_dict.get('version', False):
         response = toolkit.get_action('package_search')(
             context, {'q': 'base_name:"%s"' % base_name})
         if response['count'] > 0:
-            latest = response['results'][0]
-            try:
-                version = str(int(latest['version'])+1)
-            except ValueError:
-                version = '1'
-            data_dict['version'] = version
+            version = response['results'][0].get('version', '1')
+            if version.isdigit():
+                data_dict['version'] = str(int(version)+1)
+            else:
+                data_dict['version'] = '1'
         else:
             data_dict['version'] = '1'
     data_dict['name'] += '_v' + data_dict['version']
+
+    # create package and version
     package = ckan_package_create(context, data_dict)
     toolkit.get_action('dataset_version_create')(
         context, {
@@ -33,4 +40,5 @@ def package_create(context, data_dict):
             'owner_org': data_dict['owner_org'],
         }
     )
+
     return package
