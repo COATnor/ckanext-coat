4bdeefd7491e56f11dad0ea0d84dd1a166190ffa
Francesco Frassinelli <francesco.frassinelli@nina.no>
2019-11-02 17:46:51 +0100

    Add ckanext-scheming workaround

--- ckanext/coat/logic/action/update.py
+++ ckanext/coat/logic/action/update.py
@@ -1,6 +1,6 @@
 import ckan.plugins.toolkit as toolkit
 from ckan.logic.action.update import package_update as ckan_package_update
-from ckanext.coat import helpers
+from ckanext.coat.helpers import extras_dict, is_protected
 
 
 @toolkit.side_effect_free
@@ -8,5 +8,15 @@ def package_update(context, data_dict):
     package = toolkit.get_action('package_show')(context, data_dict)
     # check if protected only if not switching between private and public
     if data_dict.get('private', False) == package.get('private', False):
-        helpers.is_protected(package)
+        is_protected(package)
+
+    # ckanext-scheming workaround
+    base_name = extras_dict(package).get('base_name')
+    if base_name:
+        data_dict.setdefault('extras', [])
+        if 'base_name' not in extras_dict(data_dict):
+            data_dict['extras'].append(
+                {'key': 'base_name', 'value': base_name},
+             )
+
     return ckan_package_update(context, data_dict)
