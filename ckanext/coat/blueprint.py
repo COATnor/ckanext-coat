ed74b78e91e09d3bff6526e15cd4e23e31ecf0a7
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-12-28 12:32:54 +0100

    Remove ZIP bulk download

--- ckanext/coat/blueprint.py
+++ ckanext/coat/blueprint.py
@@ -2,19 +2,15 @@ import ckan.plugins.toolkit as toolkit
 import ckan.logic as logic
 import ckan.lib.base as base
 import ckan.lib.helpers as h
-from ckan.common import _, request
+from ckan.common import _
 from ckanext.coat.helpers import extras_dict, new_context, next_version, get_resource_path
 from ckanext.datasetversions.helpers import get_context
 
-from flask import Blueprint, make_response
-#import paste.fileapp
+from flask import Blueprint
 
 import copy
 import datetime
-import mimetypes
-import tempfile
 import os
-import zipfile
 
 
 coat = Blueprint('coat', __name__)
@@ -80,50 +76,4 @@ def new_version(uid):
 
     return h.redirect_to(controller='dataset', action='read', id=base_name)
 
-"""
-def _send_file(path, name):
-    # Similar to resource_download from ckan/controllers/package.py
-    fileapp = paste.fileapp.FileApp(path)
-    try:
-        status, headers, app_iter = request.call_application(fileapp)
-    except OSError:
-        base.abort(404, _('Resource data not found'))
-    response = make_response()
-    response.headers.update(dict(headers))
-    content_type, content_enc = mimetypes.guess_type(path)
-    if content_type:
-        response.headers['Content-Type'] = content_type
-        response.status = status
-    response.headers['Content-Disposition'] = 'attachment; filename="%s"' % name
-    # FILESIZE MISSING
-    return app_iter
-"""
-
-from flask import send_file
-def _send_file(path, name):
-    return send_file(path, as_attachment=True, attachment_filename=name)
-
-def zip(uid):
-    context = new_context()
-    data_dict = {'id': uid}
-
-    # check if package exists
-    try:
-        package = toolkit.get_action('package_show')(context, data_dict)
-        toolkit.get_action('resource_show')(context, package['resources'][0])
-    except (logic.NotFound, logic.NotAuthorized):
-        base.abort(404, _('Dataset not found'))
-
-    zip_path = os.path.join(tempfile.gettempdir(), package['id']+'.zip')
-    with zipfile.ZipFile(zip_path, 'w') as archive:
-        for resource in package['resources']:
-            if resource['url_type'] != 'upload':
-                continue
-            path = get_resource_path(resource)
-            archive.write(path, resource['name'])
-
-    return _send_file(zip_path, '%s.zip' % uid)
-
-
 coat.add_url_rule(u'/dataset/<uid>/new_version', view_func=new_version)
-coat.add_url_rule(u'/dataset/<uid>/zip', view_func=zip)
